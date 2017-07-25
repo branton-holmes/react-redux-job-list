b0b05ac5371c623b87505c89dc8a264e0ed3499f
IGNITE\bholmes <bholmes@ignitesales.com>
2017-07-25 13:32:30 -0500

    Helps if the favorites link goes somewhere

--- app/routes.js
+++ app/routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import AllJobs from './containers/AllJobs';
 import About from './components/About';
+import Favorites from './containers/Favorites';
 
 export default (
 	<Switch>
 		<Route exact path="/about" component={About} />
 		<Route path="/all_jobs" component={AllJobs} />
+		<Route path="/favorites" component={Favorites} />
 	</Switch>
 );
